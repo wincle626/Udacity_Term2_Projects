16ca8b215e5390ae0035b056aef794250e549be1
YunWu <wincle626>
2018-12-10 17:22:22 +0000

    add pid project

---
+++ project04/mycmake/build/CMakeFiles/PID.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PID.dir/src/PID.cpp.o"
+  "CMakeFiles/PID.dir/src/main.cpp.o"
+  "PID.pdb"
+  "PID"
+  "PID-"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PID.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
