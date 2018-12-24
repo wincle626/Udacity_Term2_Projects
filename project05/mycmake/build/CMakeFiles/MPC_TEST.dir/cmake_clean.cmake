0f612d2775529b14217da94c5341f80fbd62e946
YunWu <wincle626>
2018-12-24 12:59:19 +0000

    add project05

---
+++ project05/mycmake/build/CMakeFiles/MPC_TEST.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MPC_TEST.dir/src/main.cpp.o"
+  "CMakeFiles/MPC_TEST.dir/src/MPC.cpp.o"
+  "MPC_TEST.pdb"
+  "MPC_TEST"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MPC_TEST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
